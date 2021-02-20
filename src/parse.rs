b0f7ad60dd597cd761cdd5ed5cd97872797f4403
Odagawa <38175513+bababax11@users.noreply.github.com>
2021-02-20 09:24:33 +0900

    add proconio

--- src/parse.rs
+++ src/parse.rs
@@ -4,62 +4,10 @@ use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::path::Path;
+use proconio::input;
+use proconio::source::auto::AutoSource;
 
 use crate::structs::*;
-
-macro_rules! input {
-  (source = $s:expr, $($r:tt)*) => {
-      let mut iter = $s.split_whitespace();
-      input_inner!{iter, $($r)*}
-  };
-  ($($r:tt)*) => {
-      let mut s = {
-          use std::io::Read;
-          let mut s = String::new();
-          std::io::stdin().read_to_string(&mut s).unwrap();
-          s
-      };
-      let mut iter = s.split_whitespace();
-      input_inner!{iter, $($r)*}
-  };
-}
-
-macro_rules! input_inner {
-  ($iter:expr) => {};
-  ($iter:expr, ) => {};
-
-  ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
-      let $var = read_value!($iter, $t);
-      input_inner!{$iter $($r)*}
-  };
-
-  ($iter:expr, mut $var:ident : $t:tt $($r:tt)*) => {
-      let mut $var = read_value!($iter, $t);
-      input_inner!{$iter $($r)*}
-  };
-}
-
-macro_rules! read_value {
-  ($iter:expr, ( $($t:tt),* )) => {
-      ( $(read_value!($iter, $t)),* )
-  };
-
-  ($iter:expr, [ $t:tt ; $len:expr ]) => {
-      (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-  };
-
-  ($iter:expr, chars) => {
-      read_value!($iter, String).chars().collect::<Vec<char>>()
-  };
-
-  ($iter:expr, usize1) => {
-      read_value!($iter, usize) - 1
-  };
-
-  ($iter:expr, $t:ty) => {
-      $iter.next().unwrap().parse::<$t>().expect("Parse error")
-  };
-}
 
 // A simple implementation of `% cat path`
 // `% cat path`のシンプルな実装
@@ -91,7 +39,7 @@ pub fn triangles_parse(path: &Path) -> Vec<Triangle> {
 
     for s in cat(&path).unwrap().split("\r\n").filter(|s| s.len() >= 3) {
         input!(
-            source = s,
+            from AutoSource::from(s),
             _t: String,
             t: usize,
             _a: String,
@@ -115,7 +63,7 @@ pub fn parse_result(path: &Path) -> HashMap<usize, Vec<Feature>> {
     let mut results = HashMap::<usize, Vec<Feature>>::with_capacity(10000);
     for s in cat(&path).unwrap().split("\r\n").filter(|s| s.len() >= 3) {
         input!(
-            source = s,
+            from AutoSource::from(s),
             photo_no: usize,
             _is_f: i8,
             pos_a: [usize; 2],
