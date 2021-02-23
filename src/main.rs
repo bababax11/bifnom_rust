fc1ae8cea4186371511bf4954a6ec9c3b37da3bf
bababax11 <38175513+bababax11@users.noreply.github.com>
2021-02-23 16:00:17 +0900

    fix: dirs filter

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,15 @@ use std::io::prelude::*;
 use std::path::Path;
 use std::{fs, io};
 
-fn read_dir<P: AsRef<Path>>(path: P) -> io::Result<Vec<String>> {
-    Ok(fs::read_dir(path)?
-        .filter_map(|entry| {
-            let entry = entry.ok()?;
-            if entry.file_type().ok()?.is_file() {
-                Some(entry.file_name().to_string_lossy().into_owned())
-            } else {
-                None
-            }
-        })
-        .collect())
+fn read_dir<P: AsRef<Path>>(path: P) -> io::Result<impl Iterator<Item = String>> {
+    Ok(fs::read_dir(path)?.filter_map(|entry| {
+        let entry = entry.ok()?;
+        if entry.file_type().ok()?.is_file() {
+            Some(entry.file_name().to_string_lossy().into_owned())
+        } else {
+            None
+        }
+    }))
 }
 
 fn format_vec_strs(prj: &[ProjectedPix], qs: &[Quaternion], b_qs: &[Quaternion]) -> String {
@@ -33,8 +31,10 @@ fn format_vec_strs(prj: &[ProjectedPix], qs: &[Quaternion], b_qs: &[Quaternion])
 fn main() {
     const N: usize = 249 - 1;
     const BASE_PATH: &str = "RIFNOM_TAVE015_TVAR005_TANG025_TDIFF12_TTRACK6";
-    let dirs = read_dir(BASE_PATH).unwrap();
-    let mut tris = triangles_parse(concat!("Input_armadillo/tri/triangles_{:05}.txt", 0).as_ref());
+    let dirs = read_dir(BASE_PATH)
+        .unwrap()
+        .filter(|x| x.starts_with("result"));
+    let mut tris = triangles_parse(format!("Input_armadillo/tri/triangles_{:05}.txt", 0).as_ref());
 
     for dir in dirs {
         let feats = result_parse(format!("{}/{}", BASE_PATH, dir).as_ref());
@@ -44,15 +44,16 @@ fn main() {
             let result_path_str = format!("results/{}_{}", BASE_PATH, i);
             let mut file = fs::File::create(result_path_str).unwrap();
 
-            let neighbors =
-                neighbor_parse(format!("Input_armadillo/tri/triangles_{:05}.txt", i).as_ref());
+            let neighbors = neighbor_parse(
+                format!("Input_armadillo/nei/neighbor_points_{:05}.txt", i).as_ref(),
+            );
             let tris_next =
                 triangles_parse(format!("Input_armadillo/tri/triangles_{:05}.txt", i + 1).as_ref());
             let (prj, qs) = run(&feats[&i], &neighbors, &tris, &tris_next);
             let b_qs: Vec<_> = feats[&i].iter().map(|x| x.rotate_q()).collect();
 
             let results_str = format_vec_strs(&prj, &qs, &b_qs);
-            writeln!(file, "{}", results_str);
+            writeln!(file, "{}", results_str).unwrap();
 
             tris = tris_next;
         }
